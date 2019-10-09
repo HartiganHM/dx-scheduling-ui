4730d2fc9234cb3a5d146c293d48608a5149cda8
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-10-09 07:58:56 -0600

    Fix linting errors in App

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,22 @@ import client from './configuration/client';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+const App: React.FC = () => (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-        </header>
-      </div>
-    </ApolloProvider>
-  );
-}
+      </header>
+    </div>
+  </ApolloProvider>
+);
 
 export default App;
