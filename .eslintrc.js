3eb532ce8967368120f5f5bf530ff91fb978681a
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-10-09 07:59:25 -0600

    Build eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jasmine: true,
+    jest: true,
+  },
+  extends: ["airbnb-typescript"],
+  globals: {
+    $: true,
+    $$: true,
+    Atomics: 'readonly',
+    browser: true,
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-plusplus': 'off',
+    'react/forbid-prop-types': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'react/button-has-type': 'off',
+    'react/default-props-match-prop-type': 'off',
+    'react/destructuring-assignment': [
+      'error',
+      'always',
+      { ignoreClassFields: true },
+    ],
+    'id-length': ['error', { min: 3, exceptions: ['id'] }]
+  }
+}
