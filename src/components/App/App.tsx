8415846a66746cac7231e81439f1f5bd6e38564b
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-10-11 07:39:16 -0600

    Migrate App to /components; build /pages directory

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 
-import client from './configuration/client';
-import logo from './logo.svg';
+import client from '../../configuration/client';
+import logo from '../../logo.svg';
 import './App.css';
 
 const App: React.FC = () => (
