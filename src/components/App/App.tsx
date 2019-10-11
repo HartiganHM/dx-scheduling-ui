e3c74ee55e3ae48a39e192a2cf6403f910bc5212
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-10-11 08:39:59 -0600

    Upate App to render Routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,24 +2,12 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import client from '../../configuration/client';
-import logo from '../../logo.svg';
+import Routes from '../Routes/Routes';
 import './App.css';
 
 const App: React.FC = () => (
   <ApolloProvider client={client}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   </ApolloProvider>
 );
 
