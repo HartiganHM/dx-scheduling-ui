610ce6bdf18278e741d6d9f4b1883a18165dcf5b
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-31 08:34:53 -0700

    Implements StateProvider in index.tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import App from './App/App';
 import NavigationContainer from './NavigationContainer/NavigationContainer';
 import Routes from './Routes/Routes';
+import { StateProvider, useStateValue } from './StateProvider/StateProvider';
 
-export { App, NavigationContainer, Routes };
+export { App, NavigationContainer, Routes, StateProvider, useStateValue };
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 
-import client from './configuration/client';
-import App from './components/App/App';
+import client from 'configuration/client';
+import { App, StateProvider } from 'components';
+import { initialState, globalReducer } from 'shared/data';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <StateProvider initialState={initialState} reducer={globalReducer}>
+      <App />
+    </StateProvider>
   </ApolloProvider>,
   document.getElementById('root')
 );
