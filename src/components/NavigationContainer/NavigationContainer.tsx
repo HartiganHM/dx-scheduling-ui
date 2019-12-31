7f1584c1b51c7cc79e2f68f4d0e9941fc776da36
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-30 18:58:50 -0700

    Mocks out NavigationContainer

--- src/components/NavigationContainer/NavigationContainer.tsx
+++ src/components/NavigationContainer/NavigationContainer.tsx
@@ -1,18 +1,73 @@
 import React, { ReactChild } from 'react';
-import { History } from 'history';
+import { useHistory } from 'react-router-dom';
+import {
+  HeaderMenu,
+  SideNavigation,
+} from '@f-design/component-library';
 
 import './NavigationContainer.scss';
 
 type NavigationContainerProps = {
-  history: History;
   children: ReactChild | ReactChild[];
 };
 
-const NavigationContainer = ({ history, children }: NavigationContainerProps) => {
+const NavigationContainer = ({ children }: NavigationContainerProps) => {
+  const history = useHistory();
+
+  const sideNavigationMenuOptions = {
+    user: {
+      icon: 'person',
+      titleType: 'subOption option',
+      subOptions: ['my info', 'notifications', 'logout'],
+    },
+    settings: {
+      icon: 'settings_applications',
+      titleType: 'subOption',
+      subOptions: ['preferences', 'help', 'feedback'],
+    },
+  };
+
+  const currentlyViewing = {
+    path: '/user/my info',
+    title: 'user',
+  };
+
   console.log({ history });
   return (
-    <div>
-      {children}
+    <div className="dx-navigation-container">
+      <SideNavigation
+        currentlyViewing={currentlyViewing}
+        menuOptions={sideNavigationMenuOptions}
+        defaultSelected={{
+          option: 'user',
+          subOption: 'my info',
+        }}
+      />
+
+      <div className="dx-navigation-container__content">
+        <HeaderMenu
+          currentlyViewing={{
+            title: 'Development FX',
+            subTitle: 'Scheduling Application',
+            path: '/best-site',
+          }}
+          menuOptions={{
+            settings: {
+              icon: 'settings',
+              path: '/settings',
+            },
+            notifications: {
+              icon: 'notifications',
+              path: '/notifications',
+            },
+            search: {
+              icon: 'search',
+            },
+          }}
+          defaultTitle="Dashboard"
+        />
+        {children}
+      </div>
     </div>
   );
 };
