57929ba52181dfd3a9b978a696a502a0b3b535a3
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-12-31 08:28:30 -0700

    Builds global state types

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,3 +1,18 @@
+// Global State
+export type Actions = {
+  type: ActionTypeEnum;
+  currentlyViewing?: CurrentlyViewing;
+};
+
+export enum ActionTypeEnum {
+  UpdateCurrentlyViewing = 'UPDATE_CURRENTLY_VIEWING',
+}
+
+export interface InitialState {
+  currentlyViewing?: CurrentlyViewing;
+}
+
+// Navigation
 export interface CurrentlyViewing {
   path: string;
   title: string;
