a7502d56425b03f6ad39a84a51d572e6b9369b0d
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-01-07 22:49:37 -0700

    Builds currenlyViewingByPathEnum

---
+++ src/shared/enums/currentlyViewingByPathEnum.ts
@@ -0,0 +1,13 @@
+const currentlyViewingByPathEnum: any = {
+  '/administration': {
+    title: 'User Administation',
+  },
+  '/intake': {
+    title: 'Intake Form',
+  },
+  '/permissions': {
+    title: 'Permissions',
+  },
+};
+
+export default currentlyViewingByPathEnum;
