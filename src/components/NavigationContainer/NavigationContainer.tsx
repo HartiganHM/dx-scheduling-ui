a9d4f4116e73a1a020e0a7f92610fae6c9a008cd
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-01-19 20:30:58 -0700

    Update NavigationContainer styles/props

--- src/components/NavigationContainer/NavigationContainer.tsx
+++ src/components/NavigationContainer/NavigationContainer.tsx
@@ -43,7 +43,7 @@ const NavigationContainer: FunctionComponent<NavigationContainerProps> = ({
         }
         defaultSelected={{
           option: 'user',
-          subOption: 'my info',
+          subOption: 'profile',
         }}
       />
 
