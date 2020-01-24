14d8670c0e2f604a8b2b01b4fea101809ea254fa
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-01-23 20:26:55 -0700

    Build insuranceHandlers

---
+++ src/pages/IntakeForm/utilities/insuranceHandlers.ts
@@ -0,0 +1,75 @@
+import { ChangeEvent } from 'react';
+
+import { defaultInsuranceValues } from 'shared/data';
+import { InsuranceType, ParentType } from 'shared/types/types';
+
+const handleSelectInsurance = (
+  { target: { name, checked } }: ChangeEvent<HTMLInputElement>,
+  insurances: InsuranceType[]
+): InsuranceType[] => {
+  const newInsurances = checked
+    ? [
+        ...insurances,
+        {
+          ...defaultInsuranceValues,
+          provider: name,
+        },
+      ]
+    : insurances.filter(({ provider }) => provider !== name);
+
+  return newInsurances;
+};
+
+const handleUpdateInsuranceByIndex = (
+  insurances: InsuranceType[],
+  index: number,
+  property: string,
+  value: string | boolean | ParentType
+): InsuranceType[] =>
+  insurances.map((parent, idx) => {
+    if (index === idx) {
+      return {
+        ...parent,
+        [property]: value,
+      };
+    }
+
+    return parent;
+  });
+
+const handleUpdateInsuranceInputValues = (
+  { target: { name, value } }: ChangeEvent<HTMLInputElement>,
+  index: number,
+  insurances: InsuranceType[]
+): InsuranceType[] => {
+  const newInsurances = handleUpdateInsuranceByIndex(
+    insurances,
+    index,
+    name,
+    value
+  );
+
+  return newInsurances;
+};
+
+const handleUpdateInsured = (
+  value: string,
+  index: number,
+  insurances: InsuranceType[]
+): InsuranceType[] => {
+  const newInsurances = handleUpdateInsuranceByIndex(
+    insurances,
+    index,
+    'insured',
+    value
+  );
+
+  return newInsurances;
+};
+
+export {
+  handleSelectInsurance,
+  handleUpdateInsuranceByIndex,
+  handleUpdateInsuranceInputValues,
+  handleUpdateInsured,
+};
