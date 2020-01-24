a547f6e995fc686cb7936ca5d86cd68ac246a604
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-01-23 20:00:52 -0700

    Update globalReducer for intakeFormValues

--- src/shared/data/globalReducer.ts
+++ src/shared/data/globalReducer.ts
@@ -5,12 +5,23 @@ import { Actions, ActionTypesEnum, InitialState } from 'shared/types/types';
 const globalReducer: Reducer<InitialState, Actions> = (state, action) => {
   switch (action.type) {
     case ActionTypesEnum.UpdateCurrentlyViewing: {
-      return {
-        ...state,
-        currentlyViewing: action.currentlyViewing,
-      };
+      const { currentlyViewing } = action;
+      if (currentlyViewing) {
+        return {
+          ...state,
+          currentlyViewing,
+        };
+      }
     }
-
+    case ActionTypesEnum.UpdateIntakeValues: {
+      const { intakeFormValues } = action;
+      if (intakeFormValues) {
+        return {
+          ...state,
+          intakeFormValues: intakeFormValues,
+        };
+      }
+    }
     default: {
       return state;
     }
