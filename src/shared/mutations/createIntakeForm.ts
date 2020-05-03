b9db25c54d65f43a2defdcef1a3a4b3a36e590b3
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-05-02 18:14:47 -0600

    Update input example for createIntakeForm mutation

--- src/shared/mutations/createIntakeForm.ts
+++ src/shared/mutations/createIntakeForm.ts
@@ -4,58 +4,97 @@ import gql from 'graphql-tag';
 * Input Example
 {
   "input": {
-    "date": String!
-    "servicesRequested": {
-      "set": [String!]
-    },
-    "client": {
+    "intakeFormValues": {
       "create": {
-        "firstName": String!
-    		"lastName": String!
-    		"dob": String!
-    		"gender": String!
-    		"school": String!
-    		"grade": String!
-    		"parents": {
-      		"create": [
-        		{
-          		"firstName": String!
-          		"lastName": String!
-          		"phoneNumber": String!
-          		"email": String!
-          		"isInSameHousehold": Boolean!,
-          		"address": {
-            		"create": {
-              		"street": String!
-              		"city": String!
-              		"state": String!
-              		"zip": String!
-            		}
-          		}	
-        		}
-      		]
-        },
-        "insurances": {
+          "date": String!
+          "servicesRequested": {
+            "set": [String!]
+          },
+          "client": {
+            "create": {
+              "firstName": String!
+              "lastName": String!
+              "dob": String!
+              "gender": String!
+              "school": String!
+              "grade": String!
+              "parents": {
+                "create": [
+                  {
+                    "firstName": String!
+                    "lastName": String!
+                    "phoneNumber": String!
+                    "email": String!
+                    "isInSameHousehold": Boolean!,
+                    "address": {
+                      "create": {
+                        "street": String!
+                        "city": String!
+                        "state": String!
+                        "zip": String!
+                      }
+                    }
+                  }
+                ]
+              },
+              "insurances": {
+                "create": {
+                  "idNumber": String!
+                  "groupNumber": String!
+                  "provider": String!
+                  "insured": String!
+                }
+              },
+              "physician": {
+                "create": {
+                  "firstName": String!
+                  "lastName": String!
+                  "practice": String!
+                  "phoneNumber": String!
+                }
+              }
+            }
+          }
+        }
+      }
+    intakeFormQuestions": {
+      "create": {
+        "creditCardInfoSaved": Boolean!
+        "ratesDiscussed": Boolean!
+        "preferredTimes": String!
+        "needs": String!
+        "hasReferral": Boolean!
+        "priorTherapy": String!
+        "schoolSupport": String!
+        "priorTreatments": String!
+        "referralConcernMatch": Boolean
+        "diagnosis": {
           "create": {
-            "idNumber": String!
-            "groupNumber": String!
+            "name": String!
             "provider": String!
-            "insured": String!
+            "date": String!
+            "comments": String!
           }
         },
-        "physician": {
+        "referral": {
           "create": {
             "firstName": String!
             "lastName": String!
-            "practice": String!
-            "phoneNumber": String!
+          }
+        },
+        "concerns": {
+          "create": {
+            "areas": String
+            "communication": String
+            "motor": String
+            "sensory": String
+            "cognitive": String
           }
         }
       }
     }
   }
 }
-*/
 
 const CREATE_INTAKE_FORM = gql`
   mutation createIntakeFormValues($input: IntakeFormValuesCreateInput!) {
